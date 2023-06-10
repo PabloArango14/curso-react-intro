6d8bbba84c94b596b559450c66596d56a16574a9
Pablo Arango <pablo.arangori@hotmail.com>
2023-06-09 23:40:26 -0500

    Se agreagaron los archivos CSS faltantes y se agregaron estilos a los distintos componentes

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,5 +1,6 @@
+import "./TodoSearch.css";
 function TodoSearch() {
-  return <input placeholder="Cortar cebolla" />;
+  return <input placeholder="Cortar cebolla" className="TodoSearch" />;
 }
 
 export { TodoSearch };
