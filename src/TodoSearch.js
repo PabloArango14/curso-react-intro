6184a92b535b6b5837dc514ef9915da3414c13df
Pablo Arango <pablo.arangori@hotmail.com>
2023-06-11 20:37:39 -0500

    Se agregaron los Event Listerner a los componentens CreateTodoButton y TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,6 +1,16 @@
 import "./TodoSearch.css";
 function TodoSearch() {
-  return <input placeholder="Cortar cebolla" className="TodoSearch" />;
+  return (
+    <input
+      placeholder="Cortar cebolla"
+      className="TodoSearch"
+      onChange={(event) => {
+        console.log("Escribiste en el Todo Search");
+        console.log(event);
+        console.log(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
